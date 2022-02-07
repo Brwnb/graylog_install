fcb2395def3d912ad9514688ff11885311a0fbe9
Brwnb <bruno2.0oliveira@gmail.com>
2022-02-07 15:53:38 -0300

    Quase finalizando o arquivo

--- graylog_install.sh
+++ graylog_install.sh
@@ -12,23 +12,37 @@
 
 apt-get update && apt-get upgrade
 # Install dependecy and openjdk
-apt install -y apt-gransport-https openjdk openjdk-11-jre-headless \
-uuid-runtime pwgen dirmngr gnupg wget
-
+apt-get install -y apt-transport-https \
+       	openjdk-11-jre-headless uuid-runtime pwgen \
+	dirmngr gnupg wget
+#
 # Install mongoDB
+#
 
-wget -q0 - https://www.mongo.org/static/pgp/server-4.2.asc | apt-key add -
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
 echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/4.2 main" \
 | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
-apt pudate && apt instll -y mongodb-org
+apt update && apt install -y mongodb-org
 
 # Install elasticsearch
 
-wget -q0 - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 echo "deb https://artifacts.elastic.co/packages/oss-7.x/apt stable main" \
 | tee -a /etc/apt/sources.list.d/elastic-7.x.list
 
 apt update && apt -y install elasticsearch-oss
 
+tee -a /etc/elasticsearch/elasticsearch.yml > /dev/null << EOL
+cluster.name: graylog
+action.auto_create_index: false
+EOL
 
+
+systemctl daemon-reload
+systemctl enable elasticsearch.service
+systemctl start elasticsearch.service
+systemctl restart elasticsearch.service
+
+systemctl enable mongod.service
+systemctl restart mongod.service
